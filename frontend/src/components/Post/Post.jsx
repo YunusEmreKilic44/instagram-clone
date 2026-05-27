ee4e674f6b1af468d33a27659cb054feac2219fc
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-05-27 17:19:30 +0300

    Post component

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -1,7 +1,53 @@
+import Avatar from "@mui/material/Avatar";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import "./Post.css";
 
 const Post = () => {
-  return <div>Post</div>;
+  return (
+    <div className="post-wrapper">
+      <div className="post-header">
+        <div className="post-header-left">
+          <a href="/">
+            <Avatar
+              src="/static/images/avatar/1.jpg"
+              sx={{ width: 32, height: 32 }}
+            />
+          </a>
+          <a href="/" className="profile-username">
+            eminbasbayan
+          </a>
+        </div>
+        <div className="post-header-right">
+          <button>
+            <MoreHorizIcon />
+          </button>
+        </div>
+      </div>
+      <div className="post-image">
+        <img src="/images/post/1.png" alt="Post image" />
+      </div>
+      <div className="post-bottom">
+        <div className="post-like">
+          <button>
+            <FavoriteIcon className="post-like-icon active" />
+          </button>
+        </div>
+        <span className="post-like-count">0 like</span>
+        <div className="post-content">
+          <a href="/" className="profile-username">
+            eminbasbayan
+          </a>{" "}
+          <span className="post-text">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem
+            natus pariatur repudiandae molestias repellat reiciendis accusamus
+            debitis exercitationem, ea sequi.
+          </span>
+        </div>
+        <div className="post-time">1 dakika önce</div>
+      </div>
+    </div>
+  );
 };
 
 export default Post;
