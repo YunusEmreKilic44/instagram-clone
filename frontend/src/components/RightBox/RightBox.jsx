74904d82ff5804d4d67325e3419f9544be7114c3
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-05-27 17:38:29 +0300

    RightBox component

--- frontend/src/components/RightBox/RightBox.jsx
+++ frontend/src/components/RightBox/RightBox.jsx
@@ -1,7 +1,48 @@
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import ArrowCircleRightOutlinedIcon from "@mui/icons-material/ArrowCircleRightOutlined";
 import "./RightBox.css";
 
 const RightBox = () => {
-  return <div>RightBox</div>;
+  return (
+    <div className="right-box">
+      <div className="account">
+        <a href="/">
+          <Avatar src="/images/person/0.png" sx={{ width: 56, height: 56 }} />
+        </a>
+        <div className="account-titles">
+          <a href="/">eminbasbayan</a>
+          <span>Emin Başbayan</span>
+        </div>
+      </div>
+      <span className="friends-title">My Friends</span>
+      <ul className="friends-list">
+        <li className="friend-item">
+          <div className="friend-item-left">
+            <a href="/">
+              <Avatar
+                src="/images/person/2.png"
+                sx={{ width: 40, height: 40 }}
+              />
+            </a>
+            <div className="friend-username">
+              <a href="/">bedirhan</a>
+              <span>Bedirhan Başbayan</span>
+            </div>
+          </div>
+          <div className="friend-item-right">
+            <Button
+              variant="contained"
+              endIcon={<ArrowCircleRightOutlinedIcon />}
+              size="small"
+            >
+              Profile git
+            </Button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default RightBox;
