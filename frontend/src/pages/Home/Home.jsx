fa2d7c5c5373899cdf1e84336348ad60ad121447
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-05-27 15:44:35 +0300

    initialization

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="home-page">
+        <div className="home-page-left"></div>
+        <div className="home-page-right"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
