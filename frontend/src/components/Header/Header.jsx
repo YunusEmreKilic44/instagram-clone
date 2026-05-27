cd6b9863d5108b5ffdc843b98e23e4a79b40bef4
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-05-27 16:12:15 +0300

    Header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,44 @@
+import Logo from "./Logo/Logo";
+import SearchIcon from "@mui/icons-material/Search";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import ChatOutlinedIcon from "@mui/icons-material/ChatOutlined";
+import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
+import Avatar from "@mui/material/Avatar";
+
 import "./Header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header-wrapper">
+      <div className="container">
+        <div className="header">
+          <Logo />
+          <div className="search">
+            <SearchIcon className="search-icon" />
+            <input type="text" placeholder="Search" />
+          </div>
+          <div className="header-links">
+            <a href="#">
+              <HomeOutlinedIcon className="icon" />
+            </a>
+            <a href="#">
+              <ChatOutlinedIcon className="icon" />
+            </a>
+            <a href="#">
+              <AddBoxOutlinedIcon className="icon" />
+            </a>
+            <a href="#">
+              <Avatar
+                alt="Remy Sharp"
+                src="/static/images/avatar/1.jpg"
+                sx={{ width: 28, height: 28 }}
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
---
+++ frontend/src/components/Header/Logo/Logo.jsx
@@ -0,0 +1,13 @@
+const Logo = () => {
+  return (
+    <a href="/" style={{ display: "flex" }}>
+      <img
+        className="logo"
+        src="https://cdn.worldvectorlogo.com/logos/instagram-2.svg"
+        alt="Logo"
+      />
+    </a>
+  );
+};
+
+export default Logo;
